f21ca050b45c5e81dbb7ec1ed08ae1018af40cc2
Mahmoud732 <m.ahmed732005@gmail.com>
2024-09-16 22:47:07 +0300

    Initial commit

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.singup, name='SingupPage'),
     path('singup', views.singup, name='SingupPage'),
     path('login', views.login, name='LoginPage'),
     path('forgot-password', views.forgot_password, name='ForgotPage'),
