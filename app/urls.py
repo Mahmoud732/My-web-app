4fd320490892c5d82c59ed463042d5ede29b74a5
Mahmoud732 <m.ahmed732005@gmail.com>
2024-09-17 17:03:18 +0300

    Initial commit

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('about', views.about, name='about'),
     path('singup', views.singup, name='SingupPage'),
     path('login', views.login, name='LoginPage'),
--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('product', views.product, name='product'),
-    path('products', views.products, name='products'),
+    path('', views.products, name='products'),
 ]
